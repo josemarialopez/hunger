23c816d8e1d1feda899868530c0a3ec41866615c
José María López <josemaria.lopez.palomo@gmail.com>
2021-01-16 16:34:01 +0100

    Include validations on user model

--- services/backend/users/app/models/user.rb
+++ services/backend/users/app/models/user.rb
@@ -9,5 +9,9 @@ class User
   field :email, type: String
   field :password_digest, type: String
 
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
   has_secure_password
 end
