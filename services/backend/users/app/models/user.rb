958873c3d7a15b07c08de83e293b2450f294cc54
José María López <josemaria.lopez.palomo@gmail.com>
2021-01-16 13:39:31 +0100

    Include the has_secure_password module for User model

--- services/backend/users/app/models/user.rb
+++ services/backend/users/app/models/user.rb
@@ -1,8 +1,13 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  include ActiveModel::SecurePassword
+
+  # Fields definition
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
   field :password_digest, type: String
+
+  has_secure_password
 end
