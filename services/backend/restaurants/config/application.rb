aa008d4d4cbba95e0f272e25462b06e9d95d2a77
José María López <josemaria.lopez.palomo@gmail.com>
2021-01-16 17:07:50 +0100

    Configure allowed hosts in the restaurants service

--- services/backend/restaurants/config/application.rb
+++ services/backend/restaurants/config/application.rb
@@ -33,5 +33,7 @@ module Restaurants
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts = ["hunger_restaurants_service", "localhost"]
   end
 end
