54d39c7b58046d65c338d1d2958f90fa32eb965e
José María López <josemaria.lopez.palomo@gmail.com>
2021-01-17 01:42:20 +0100

    Format rails helper

--- services/backend/users/spec/rails_helper.rb
+++ services/backend/users/spec/rails_helper.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../config/environment', __dir__)
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+
+# Load support files
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.use_active_record = false
