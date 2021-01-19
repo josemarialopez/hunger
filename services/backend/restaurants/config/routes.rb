b87787f583ca2eb63b02864aeb5d72e3a6c8d1fc
José María López <josemaria.lopez.palomo@gmail.com>
2021-01-19 21:25:55 +0100

    Adapt the routes configuration to include the categories endpoint

--- services/backend/restaurants/config/routes.rb
+++ services/backend/restaurants/config/routes.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :restaurants, only: [:index] do
+        collection do
+          get :categories
+        end
+      end
     end
   end
 end
