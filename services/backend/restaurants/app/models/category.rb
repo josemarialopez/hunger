6b5dfeca05ef5809cb0e2851e3b257a3ddec48b6
José María López <josemaria.lopez.palomo@gmail.com>
2021-01-19 21:09:51 +0100

    Create the category model

--- services/backend/restaurants/app/models/category.rb
+++ services/backend/restaurants/app/models/category.rb
@@ -2,5 +2,8 @@ class Category
   include Mongoid::Document
   include Mongoid::Timestamps
   field :name, type: String
-  field :alias, type: String
+  field :identifier, type: String
+
+  validates :name, presence: true, uniqueness: true
+  validates :identifier, presence: true, uniqueness: true
 end
